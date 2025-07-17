40c012548a37054e4a9ff671b6d8b060225a5136
Moez Ezzeddine <moez.ezzeddine@datadoghq.com>
2025-07-17 17:16:42 +0200

    first gcp tests

---
+++ gcp/variables.tf
@@ -0,0 +1,82 @@
+variable "project_id" {
+  description = "The ID of the project in which to create the VM"
+  type        = string
+}
+
+variable "region" {
+  description = "The region to deploy resources"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "The zone to deploy resources"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "network_name" {
+  description = "The name of the network"
+  type        = string
+  default     = "default"
+}
+
+variable "subnetwork_name" {
+  description = "The name of the subnetwork"
+  type        = string
+  default     = "default"
+}
+
+variable "api_key" {
+  description = "Datadog API key"
+  type        = string
+  sensitive   = true
+}
+
+variable "site" {
+  description = "Datadog site (e.g., datadoghq.com, datadoghq.eu)"
+  type        = string
+  default     = "datadoghq.com"
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key for instance access"
+  type        = string
+  default     = null
+}
+
+variable "ssh_username" {
+  description = "Username for SSH access"
+  type        = string
+  default     = "ubuntu"
+}
+
+variable "scanner_version" {
+  description = "Specifies the version of the scanner to install"
+  type        = string
+  default     = "0.11"
+  validation {
+    condition     = can(regex("^[0-9]+\\.[0-9]+", var.scanner_version))
+    error_message = "The scanner version must start with a number, followed by a period and a number (X.Y)."
+  }
+}
+
+variable "scanner_channel" {
+  description = "Specifies the channel to use for installing the scanner"
+  type        = string
+  default     = "stable"
+  validation {
+    condition     = contains(["stable", "beta", "nightly"], var.scanner_channel)
+    error_message = "The scanner channel must be either 'stable', 'beta' or 'nightly'."
+  }
+}
+
+variable "scanner_repository" {
+  description = "Repository URL to install the scanner from."
+  type        = string
+  default     = "https://apt.datadoghq.com/"
+  validation {
+    condition     = can(regex("^https://", var.scanner_repository))
+    error_message = "The scanner repository must be a valid HTTPs URL."
+  }
+}
