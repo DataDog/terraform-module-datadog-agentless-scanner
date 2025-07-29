b6d3159475c6c9d625df4f8330e7ded8b0a69e88
Moez Ezzeddine <moez.ezzeddine@datadoghq.com>
2025-07-29 14:38:37 +0200

    run tflint recursively

--- gcp/modules/agentless-scanner-service-account/variables.tf
+++ gcp/modules/agentless-scanner-service-account/variables.tf
@@ -1,8 +1,3 @@
-variable "project_id" {
-  description = "The ID of the project in which to create the resources"
-  type        = string
-}
-
 variable "impersonated_service_account_name" {
   description = "Name of the impersonated service account to grant impersonation access to"
   type        = string
