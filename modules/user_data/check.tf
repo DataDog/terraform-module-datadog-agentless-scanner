39060b065a900cd0974d15f206f5bca0ec71c764
sarra.zaghbib <sarra.zaghbib@datadoghq.com>
2025-11-27 15:23:14 +0100

    adding health check

---
+++ modules/user_data/check.tf
@@ -0,0 +1,16 @@
+check "health_check" {
+  data "http" "terraform_io" {
+    url = "https://api.${var.site}/api/v1/validate"
+
+    # Optional request headers
+    request_headers = {
+      Accept = "application/json"
+      DD-API-KEY= var.api_key
+
+    }
+  }
+  assert {
+    condition =  var.api_key == "" || data.http.terraform_io.status_code != 403
+    error_message = "The Datadog site or API key is incorrect. Please verify your configuration."
+  }
+}
